009d44c23d67e1cf8088ae3b2888c667d4c7fcfd
Jayasinghe <jay039@csiro.au>
2019-11-14 16:36:19 +1100

    add inter class test

--- pointnet/inference_test_shapenet.py
+++ pointnet/inference_test_shapenet.py
@@ -271,126 +271,127 @@ def main():
     classifier = classifier.eval()
 
     #num_batch = len(dataset) / opt.batchSize
-    correct_count = 0
-    top3_count = 0
-    total = 0
-    for count in range(len(test_dataset)):
-        print(count)
-        #print ("train accurate labels, positive and negative ", accurate_labels_positive,accurate_labels_negative)
-        #pick a random batch from the test dataset
-        points = []
-        target = []
-        
-        #create 32 pairs, where the first one contains a positive example and the rest are negative
-        for j in range(1,batch_size+1):
-            point_cloud_pair = []
-            point_cloud_pair.append(test_dataset[count][0])
-            point_cloud_pair.append(test_dataset[count][j])
-            points.append(torch.stack(point_cloud_pair))
-            if j == 1:
-                target.append([1])
-            else:
-                target.append([0])
-        points = torch.stack(points)
-        target = torch.tensor(target)
-        
-        points = points.transpose(3, 2)
-        #points_positive = points[:,:2]
-        #points_negative = points[:,0:3:2]
-        target = torch.squeeze(target)
-        #target_negative = torch.squeeze(target[:,1])
-        #print("SHape")
-        #print(target)
-        if torch.cuda.is_available():
-            points = points.cuda()
-            target = target.cuda()
-            #points_positive, points_negative = points_positive.cuda(), points_negative.cuda()
-            #target_negative, target_positive = target_negative.cuda(), target_positive.cuda()
-        
-        #print("shape of input", points_positive.shape)
-        pred, trans, trans_feat = classifier(points) # original and positive image
-        #pred_negative, trans, trans_feat = classifier(points_negative) # original and negative image
-        #loss = F.cross_entropy(pred, target)        
-        #loss_negative = F.cross_entropy(pred_negative, target_negative)
-        #loss = loss_negative + loss_positive
-        #print ("loss ", loss)
-        #print("positive prediction ", pred_positive, " positive target ", target_positive, " positive loss ", loss_positive)
-        #print("negative prediction ", pred_negative, " negative target ", target_negative, " negative loss ", loss_negative)
+    for class_id in range(num_classes):
+        correct_count = 0
+        top3_count = 0
+        total = 0
+        for count in range(len(test_dataset)/num_classes):
+            print(count)
+            #print ("train accurate labels, positive and negative ", accurate_labels_positive,accurate_labels_negative)
+            #pick a random batch from the test dataset
+            points = []
+            target = []
+            
+            #create 32 pairs, where the first one contains a positive example and the rest are negative
+            for j in range(1,batch_size+1):
+                point_cloud_pair = []
+                point_cloud_pair.append(test_dataset[count][0])
+                point_cloud_pair.append(test_dataset[count][j])
+                points.append(torch.stack(point_cloud_pair))
+                if j == 1:
+                    target.append([1])
+                else:
+                    target.append([0])
+            points = torch.stack(points)
+            target = torch.tensor(target)
+            
+            points = points.transpose(3, 2)
+            #points_positive = points[:,:2]
+            #points_negative = points[:,0:3:2]
+            target = torch.squeeze(target)
+            #target_negative = torch.squeeze(target[:,1])
+            #print("SHape")
+            #print(target)
+            if torch.cuda.is_available():
+                points = points.cuda()
+                target = target.cuda()
+                #points_positive, points_negative = points_positive.cuda(), points_negative.cuda()
+                #target_negative, target_positive = target_negative.cuda(), target_positive.cuda()
+            
+            #print("shape of input", points_positive.shape)
+            pred, trans, trans_feat = classifier(points) # original and positive image
+            #pred_negative, trans, trans_feat = classifier(points_negative) # original and negative image
+            #loss = F.cross_entropy(pred, target)        
+            #loss_negative = F.cross_entropy(pred_negative, target_negative)
+            #loss = loss_negative + loss_positive
+            #print ("loss ", loss)
+            #print("positive prediction ", pred_positive, " positive target ", target_positive, " positive loss ", loss_positive)
+            #print("negative prediction ", pred_negative, " negative target ", target_negative, " negative loss ", loss_negative)
 
-        #pred_choice = pred.data.max(1)[1]
-        #correct = pred_choice.eq(target.data).cpu().sum()
-        #print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
+            #pred_choice = pred.data.max(1)[1]
+            #correct = pred_choice.eq(target.data).cpu().sum()
+            #print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
 
-        print ("pred")
-        print (pred)
-        
-        
-        result = torch.max(pred, dim=1)
-        print("result")
-        print(result.indices)
-        #of all the positive results, pick the highest positive result and the top 3 positive results
-        #we expect the top result to be the 0th index
-        positives = []
-        positive_indices = []
-        max_index, max_index2, max_index3 = -1, -1, -1
-        #print("res", result)
-        for j in range (len(result.indices)):
-            if result.indices[j]==1:
-                positives.append(result.values[j].item())
-                positive_indices.append(j)
-        
+            print ("pred")
+            print (pred)
+            
+            
+            result = torch.max(pred, dim=1)
+            print("result")
+            print(result.indices)
+            #of all the positive results, pick the highest positive result and the top 3 positive results
+            #we expect the top result to be the 0th index
+            positives = []
+            positive_indices = []
+            max_index, max_index2, max_index3 = -1, -1, -1
+            #print("res", result)
+            for j in range (len(result.indices)):
+                if result.indices[j]==1:
+                    positives.append(result.values[j].item())
+                    positive_indices.append(j)
+            
 
-        if len(positives)>0:
-            print("max")
-            print(positives)
-            max_index = positive_indices[positives.index(max(positives))]
+            if len(positives)>0:
+                print("max")
+                print(positives)
+                max_index = positive_indices[positives.index(max(positives))]
 
-            del positive_indices[positives.index(max(positives))]
-            del positives[positives.index(max(positives))]
-            if (len(positives)>0):
-                max_index2 = positive_indices[positives.index(max(positives))]
                 del positive_indices[positives.index(max(positives))]
                 del positives[positives.index(max(positives))]
                 if (len(positives)>0):
-                    max_index3 = positive_indices[positives.index(max(positives))]
-            print ("top 3")
-            print(max_index, max_index2, max_index3)
-        else:
-            print("no result found")
+                    max_index2 = positive_indices[positives.index(max(positives))]
+                    del positive_indices[positives.index(max(positives))]
+                    del positives[positives.index(max(positives))]
+                    if (len(positives)>0):
+                        max_index3 = positive_indices[positives.index(max(positives))]
+                print ("top 3")
+                print(max_index, max_index2, max_index3)
+            else:
+                print("no result found")
 
-        if (max_index ==0):
-            correct_count+=1
-            print("result correct")
-        if ((max_index == 0) or (max_index2 == 0) or (max_index3 == 0)):
-            top3_count +=1
-            print("result in top3")
-        total+=1
-        #accurate_test_labels_positive = torch.sum(torch.argmax(pred_positive, dim=1) == target_positive).cpu()
-        #accurate_test_labels_negative = torch.sum(torch.argmax(pred_negative, dim=1) == target_negative).cpu()
-        #accurate_test_labels = accurate_test_labels_positive + accurate_test_labels_negative
-        #all_test_labels = len(target_positive) + len(target_negative)
-        #test_accuracy = 100. * float(accurate_test_labels) / float(all_test_labels)
-        #accuracy+= test_accuracy
-        #print accuracy
-        #print (accurate_test_labels_positive, accurate_test_labels_negative)
-        #accuracy = 100. * float(accurate_labels) / float(all_labels)
-        #print('Test accuracy: {}/{} ({:.3f}%)'.format(accurate_test_labels, all_test_labels, test_accuracy))
-        #outputfile.write('Test accuracy: {}/{} ({:.3f}%)'.format(accurate_test_labels, all_test_labels, test_accuracy))
-        #outputfile.write('Train accuracy: {}/{} ({:.3f}%)'.format(accurate_labels, all_labels, accuracy))
-        #print('Train accuracy: {}/{} ({:.3f}%)'.format(accurate_labels, all_labels, accuracy))
-        #outputfile.write("\n\n\n")
+            if (max_index ==0):
+                correct_count+=1
+                print("result correct")
+            if ((max_index == 0) or (max_index2 == 0) or (max_index3 == 0)):
+                top3_count +=1
+                print("result in top3")
+            total+=1
+            #accurate_test_labels_positive = torch.sum(torch.argmax(pred_positive, dim=1) == target_positive).cpu()
+            #accurate_test_labels_negative = torch.sum(torch.argmax(pred_negative, dim=1) == target_negative).cpu()
+            #accurate_test_labels = accurate_test_labels_positive + accurate_test_labels_negative
+            #all_test_labels = len(target_positive) + len(target_negative)
+            #test_accuracy = 100. * float(accurate_test_labels) / float(all_test_labels)
+            #accuracy+= test_accuracy
+            #print accuracy
+            #print (accurate_test_labels_positive, accurate_test_labels_negative)
+            #accuracy = 100. * float(accurate_labels) / float(all_labels)
+            #print('Test accuracy: {}/{} ({:.3f}%)'.format(accurate_test_labels, all_test_labels, test_accuracy))
+            #outputfile.write('Test accuracy: {}/{} ({:.3f}%)'.format(accurate_test_labels, all_test_labels, test_accuracy))
+            #outputfile.write('Train accuracy: {}/{} ({:.3f}%)'.format(accurate_labels, all_labels, accuracy))
+            #print('Train accuracy: {}/{} ({:.3f}%)'.format(accurate_labels, all_labels, accuracy))
+            #outputfile.write("\n\n\n")
 
-        #torch.save(classifier.state_dict(), '%s/cls_model_partial_ransac_lr_0001_partial_radius_1_parallel_%d.pth' % (opt.outf, epoch))
-    print ("total")
-    print(total)
-    print("Correct")
-    print(correct_count)
-    print("top3")
-    print(top3_count)
-    print ("Accuracy")
-    print(100. * float(correct_count)/float(total))
-    print ("Top 3 Accuracy")
-    print(100. * float(top3_count)/float(total))
+            #torch.save(classifier.state_dict(), '%s/cls_model_partial_ransac_lr_0001_partial_radius_1_parallel_%d.pth' % (opt.outf, epoch))
+        print ("total")
+        print(total)
+        print("Correct")
+        print(correct_count)
+        print("top3")
+        print(top3_count)
+        print ("Accuracy")
+        print(100. * float(correct_count)/float(total))
+        print ("Top 3 Accuracy")
+        print(100. * float(top3_count)/float(total))
     
     
     #print("final accuracy")
